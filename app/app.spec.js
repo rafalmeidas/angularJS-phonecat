07e6561b18c879a0a9ec7696f9f564a87ce7ad6d
Rafael Silva <rafael.almeida@petize.com.br>
2024-05-15 21:06:26 -0300

    feat: creating new modules

---
+++ app/app.spec.js
@@ -0,0 +1,21 @@
+describe("PhoneListController", function () {
+    beforeEach(module("phonecatApp"));
+
+    it("should create a `phones` model with 3 phones", inject(function (
+        $controller,
+    ) {
+        var scope = {};
+        var ctrl = $controller("PhoneListController", { $scope: scope });
+
+        expect(scope.phones.length).toBe(3);
+    }));
+
+    it("should create a `numberPhones` model with phones array", inject(function (
+        $controller,
+    ) {
+        var scope = {};
+        var ctrl = $controller("PhoneListController", { $scope: scope });
+
+        expect(scope.numberPhones).toBe(3);
+    }));
+});
