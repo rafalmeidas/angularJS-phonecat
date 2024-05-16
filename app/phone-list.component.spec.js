952146711144957d1de755efb284bc99f830b9dc
Rafael Silva <rafael.almeida@petize.com.br>
2024-05-15 23:00:19 -0300

    feat: add introduction to componentization

---
+++ app/phone-list.component.spec.js
@@ -0,0 +1,13 @@
+describe("phoneList", function () {
+    beforeEach(module("phonecatApp"));
+
+    describe("PhoneListController", function () {
+        it("should create a `phones` model with 3 phones", inject(function (
+            $componentController,
+        ) {
+            var ctrl = $componentController("phoneList");
+
+            expect(ctrl.phones.length).toBe(3);
+        }));
+    });
+});
